e5b1c42022737dfed72ed3042fa288c238635c6a
HP <shetloorrahul9@gmail.com>
2025-04-29 17:48:10 +0530

    created by team leader new repo

---
+++ Git_hub/src/main/java/com/sr/Git_hub/App.java
@@ -0,0 +1,10 @@
+package com.sr.Git_hub;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Git_hub/src/test/java/com/sr/Git_hub/AppTest.java
@@ -0,0 +1,19 @@
+package com.sr.Git_hub;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
